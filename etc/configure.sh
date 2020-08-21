51af5031388f3f7c2910cac992b63f79e49eb4fb
hacktk <hacktk3@gmail.com>
2020-08-21 14:05:33 +0900

    Add yay

--- etc/configure.sh
+++ etc/configure.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# yay
+YAY_PATH="/tmp/yay"
+mkdir -p $YAY_PATH && cd $YAY_PATH
+git clone https://aur.archlinux.org/yay.git ./
+makepkg -si
+cd ~ && rm -rf $YAY_PATH
+
 # gsettings
 gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
