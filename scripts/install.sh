23db3581c930a2fd6d24d1745887847e58cd4f57
hacktk <hacktk3@gmail.com>
2020-09-04 12:18:23 +0900

    Add yay

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,106 +2,74 @@
 
 set -eux
 
+# AUR helper (yay)
 CUR_PATH=$(pwd)
+YAY_PATH="/tmp/yay"
+mkdir -p $YAY_PATH && cd $YAY_PATH
+git clone https://aur.archlinux.org/yay.git ./
+makepkg -cirs --needed --noconfirm
+cd $CUR_PATH && rm -rf $YAY_PATH
 
 # aws-cli
-sudo pacman -S --needed --noconfirm aws-cli
+ysy -S --needed --noconfirm aws-cli
 
 # bash completion
-sudo pacman -S --needed --noconfirm bash-completion
-
-# dash-to-dock
-DOCK_PATH="/tmp/dash-to-dock"
-mkdir -p $DOCK_PATH && cd $DOCK_PATH
-git clone https://aur.archlinux.org/gnome-shell-extension-dash-to-dock.git ./
-makepkg -cirs --needed --noconfirm
-cd $CUR_PATH && rm -rf $DOCK_PATH
+yay -S --needed --noconfirm bash-completion
 
 # dbus-action
-DBUS_PATH="/tmp/dbus-action"
-mkdir -p $DBUS_PATH && cd $DBUS_PATH
-git clone https://aur.archlinux.org/dbus-action.git ./
-makepkg -cirs --needed --noconfirm
-cd $CUR_PATH && rm -rf $DBUS_PATH
+yay -S --needed --noconfirm dbus-action
 
 # docker
-sudo pacman -S --needed --noconfirm docker docker-compose
+yay -S --needed --noconfirm docker docker-compose
 systemctl enable docker
 
 # fcitx, mozc
-sudo pacman -S --needed --noconfirm fcitx fcitx-configtool fcitx-mozc fcitx-qt5
+yay -S --needed --noconfirm fcitx fcitx-configtool fcitx-mozc fcitx-qt5
 
 # gestures
-GESTURES_PATH="/tmp/gestures"
-mkdir -p $GESTURES_PATH && cd $GESTURES_PATH
-git clone https://aur.archlinux.org/libinput-gestures.git ./
-makepkg -cirs --needed --noconfirm
-cd $CUR_PATH && rm -rf $GESTURES_PATH
+yay -S --needed --noconfirm libinput-gestures
 
-# git
-sudo pacman -S --needed --noconfirm git
+# gnome-shell-extension-dash-to-dock
+yay -S --needed --noconfirm gnome-shell-extension-dash-to-dock
 
-# go
-sudo pacman -S --needed --noconfirm go
+# gnome-system-monitor
+yay -S --needed --noconfirm gnome-system-monitor
 
 # google chrome
-CHROME_PATH="/tmp/chrome"
-mkdir -p $CHROME_PATH && cd $CHROME_PATH
-git clone https://aur.archlinux.org/google-chrome.git ./
-makepkg -cirs --needed --noconfirm
-cd $CUR_PATH && rm -rf $CHROME_PATH
+yay -S --needed --noconfirm google-chrome
 
 # hugo
-sudo pacman -S --needed --noconfirm hugo
+yay -S --needed --noconfirm hugo
+
+# ksnip
+yay -S --needed --noconfirm ksnip
+
+# mysql-workbench
+yay -S --needed --noconfirm mysql-workbench
 
 # noto
-sudo pacman -S --needed --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji
+yay -S --needed --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji
 
 # php
-sudo pacman -S --needed --noconfirm php
+yay -S --needed --noconfirm php
 
 # ricty
-sudo pacman -S --needed --noconfirm fontforge ttf-inconsolata
-
-MIGU_PATH="/tmp/migu"
-mkdir -p $MIGU_PATH && cd $MIGU_PATH
-git clone https://aur.archlinux.org/ttf-migu.git ./
-makepkg -cirs --needed --noconfirm
-cd $CUR_PATH && rm -rf $MIGU_PATH
-
-RICTY_PATH="/tmp/ricty"
-mkdir -p $RICTY_PATH && cd $RICTY_PATH
-git clone https://aur.archlinux.org/ttf-ricty.git ./
-makepkg -cirs --needed --noconfirm
-cd $CUR_PATH && rm -rf $RICTY_PATH
+yay -S --needed --noconfirm fontforge ttf-inconsolata ttf-migu ttf-ricty
 
 # terraform
-sudo pacman -S --needed --noconfirm terraform
+yay -S --needed --noconfirm terraform
 
 # vscode
-VSCODE_PATH="/tmp/vscode"
-mkdir -p $VSCODE_PATH && cd $VSCODE_PATH
-git clone https://aur.archlinux.org/visual-studio-code-bin.git ./
-makepkg -cirs --needed --noconfirm
-cd $CUR_PATH && rm -rf $VSCODE_PATH
+yay -S --needed --noconfirm visual-studio-code-bin
 
 # vlc
-sudo pacman -S --needed --noconfirm vlc
+yay -S --needed --noconfirm vlc
 
 # vpn
-sudo pacman -S --needed --noconfirm xl2tpd strongswan
-NML2TP_PATH="/tmp/nml2tp"
-mkdir -p $NML2TP_PATH && cd $NML2TP_PATH
-git clone https://aur.archlinux.org/networkmanager-l2tp.git ./
-makepkg -cirs --needed --noconfirm
-cd $CUR_PATH && rm -rf $NML2TP_PATH
+yay -S --needed --noconfirm xl2tpd strongswan networkmanager-l2tp
 
 # xrandr
-sudo pacman -S --needed --noconfirm xorg-xrandr
+yay -S --needed --noconfirm xorg-xrandr
 
 # zoom
-ZOOM_PATH="/tmp/zoom"
-mkdir -p $ZOOM_PATH && cd $ZOOM_PATH
-git clone https://aur.archlinux.org/zoom.git ./
-makepkg -cirs --needed --noconfirm
-cd $CUR_PATH && rm -rf $ZOOM_PATH
+yay -S --needed --noconfirm zoom
