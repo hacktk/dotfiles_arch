dae2ea90c71a05b21d7f8bc3122d8019f780cd2f
hacktk <hacktk3@gmail.com>
2022-09-20 16:26:15 +0900

    Add touchpad speed setting

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -30,6 +30,7 @@ gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
 gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 30
 gsettings set org.gnome.desktop.peripherals.keyboard delay 200
 gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
+gsettings set org.gnome.desktop.peripherals.touchpad speed 1.0
 gsettings set org.gnome.desktop.peripherals.touchpad send-events 'enabled'
 
 # power
